4595aeeb1f9c3a374021921c1f48ebf3fb9c9def
OmarHusseini <omar.alhussieni55@gmail.com>
2025-03-19 00:31:34 +0200

    finally dojo weather is ready

--- webfundementel/JS/DojoPizzera/test.js
+++ webfundementel/JS/DojoPizzera/test.js
@@ -14,12 +14,12 @@ function pizzaoven(hand, protein, cheese, toppings) {
 // console.log(order1);
 console.log(pizzaoven(pizza.crust));
 
-// var order2 = pizzaoven("hand tossed", "marinara", ["mozzarella ", "feta"], ["mushrooms", "olives", "onions"])
-// console.log(order2);
-// var order3 = pizzaoven("sowerdo", "katchap", ["mozzarella ", "feta"], ["olives", "tomato"])
-// var order4 = pizzaoven("tosseh", "marinara", ["mozzarella ", "feta"], ["chicken", "whitesauce"])
+var order2 = pizzaoven("hand tossed", "marinara", ["mozzarella ", "feta"], ["mushrooms", "olives", "onions"])
+console.log(order2);
+var order3 = pizzaoven("sowerdo", "katchap", ["mozzarella ", "feta"], ["olives", "tomato"])
+var order4 = pizzaoven("tosseh", "marinara", ["mozzarella ", "feta"], ["chicken", "whitesauce"])
 
-// function random_pizz(order1) {
-//     return order1[math.pizza(math.random() * order1)]
-// }
-// var doos = []
+function random_pizz(order1) {
+    return order1[math.pizza(math.random() * order1)]
+}
+var doos = []
--- webfundementel/JS/weather/weather.js
+++ webfundementel/JS/weather/weather.js
@@ -1,7 +1,4 @@
-function findall(){
-    var x = document.querySelectorAll(".celsiusright")
-    console.log(x)
-}
+
 // this is function is example
 function alertme(el) {
     // var x=document.querySelector('.country')
@@ -16,9 +13,45 @@ function removewindow() {
 
 }
 
-function convertfahrenhiet(n) {
-    var f = document.querySelectorAll('.celsius , .celsiusright ')
-    f = ((n * 9.0 / 5.0) + 32.0);
+function convert_tempreture() {
+    var tr = document.querySelectorAll(".temp_right")
+    var tl = document.querySelectorAll(".temp_left")
+    var select = document.querySelector("#type_temp")
+    // console.log(tl); 
+    /* Here tr is sympolizes of temp_right clg of(tr) not innertext */
+
+    for (var i = 0; i < tr.length; i++) {
+        if (select.value === "c") {
+            var result = ((tr[i].innerText) - 32) * (5 / 9)
+            console.log(tr[i].innerText);
+            tr[i].innerText = Math.trunc(result)
+
+        }
+        else {
+            var result = ((tr[i].innerText) * (9 / 5)) + 32
+            console.log(tr[i].innerText);
+            tr[i].innerText = Math.trunc(result)
+
+
+
+        }
+    }
+
+    for (var i = 0; i < tl.length; i++) {
+        if (select.value == "c") {
+            var result = ((tl[i].innerText) - 32) * (5 / 9)
+            console.log(tl[i].innerText);
+            tl[i].innerText = Math.trunc(result)
+
+        }
+        else {
+            var result = ((tl[i].innerText) * (9 / 5)) + 32
+            console.log(tr[i].innerText);
+            tl[i].innerText = Math.trunc(result)
+
+
+        }
+    }
 
 
 }
