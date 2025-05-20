97e6796e278a699700f6490c9c47e8ef86d1918c
OmarHusseini <omar.alhussieni55@gmail.com>
2025-05-20 14:22:50 +0300

    ready

---
+++ java-stack/Coffeeapp/TestOrders.java
@@ -0,0 +1,19 @@
+
+public class TestOrders {
+
+    public static void main(String[] args) {
+
+        CoffeKiosk shop=new CoffeKiosk();
+
+        shop.addMenuItem("cappuccino", 15);
+        shop.addMenuItem("latte", 12);
+        shop.addMenuItem("americano", 10);
+        shop.addMenuItem("chai latte", 12);
+        shop.addMenuItem("espresso", 10);
+        shop.addMenuItem("turkish coffee", 12);
+
+        shop.displayMenu();
+        shop.newOrder();
+        
+    }
+}
