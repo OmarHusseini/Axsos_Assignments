3d4877ec4a49119207d96a28f7c41e84b45d9c2f
OmarHusseini <omar.alhussieni55@gmail.com>
2025-06-05 13:05:00 +0300

    assignment that raleted with database

---
+++ java-stack/savetarvel/src/main/java/com/axsosacademy/savetarvel/SavetarvelApplication.java
@@ -0,0 +1,13 @@
+package com.axsosacademy.savetarvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavetarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavetarvelApplication.class, args);
+	}
+
+}
---
+++ java-stack/savetarvel/src/test/java/com/axsosacademy/savetarvel/SavetarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsosacademy.savetarvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavetarvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
